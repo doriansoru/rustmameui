075c85174c20a5d9375c26aa0bf60e69551943ff
Dorian Soru <doriansoru@gmail.com>
2025-04-29 10:36:24 +0200

    Updated the documentation

--- doc/rustmameui/ui/sidebar-items.js
+++ doc/rustmameui/ui/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["ABOUT","APP_NAME","BATCH_SIZE","TABS_CSS"],"enum":["Tab"],"fn":["App","check_dialog_utility","draw"]};
+window.SIDEBAR_ITEMS = {"constant":["ABOUT","APP_NAME","BATCH_SIZE","TABS_CSS"],"enum":["Tab"],"fn":["AboutTab","App","FavouritesTab","GamesTab","SettingsTab","check_dialog_utility","draw"],"struct":["FavouritesTabProps","GamesTabProps","SettingsTabProps"]};
